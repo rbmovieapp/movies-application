1b67d363b65708d6e0faa811d2b46a05bc6bf9f6
Rachel Castaneda <rachel.castaneda03@gmail.com>
2020-01-23 10:54:07 -0600

    changed table and get request

--- src/index.js
+++ src/index.js
@@ -1,88 +1,55 @@
+const $ = require('jquery');
+
 /**
  * es6 modules and imports
  */
 import sayHello from './hello';
 sayHello('World');
 
+
 /**
  * require style imports
  */
-const {getMovies} = require('./api.js');
-
-getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
-});
--------------------------------------------------------------------------------
-
-
-<table id="products">
-    <thead>
-    <tr>
-    <th>Title</th>
-    <th>Quantity</th>
-    <th>Price</th>
-    <th>Categories</th>
-    </tr>
-    </thead>
-    <tbody id="insertProducts"></tbody>
-    </table>
-    <button id = "btn">Refresh</button>
+// const {getMovies} = require('./api.js');
+//
+// getMovies().then((movies) => {
+//   console.log('Here are all the movies:');
+//   movies.forEach(({title, rating, id}) => {
+//     console.log(`id#${id} - ${title} - rating: ${rating}`);
+//   });
+// }).catch((error) => {
+//   alert('Oh no! Something went wrong.\nCheck the console for details.')
+//   console.log(error);
+// });
+// -------------------------------------------------------------------------------
 
 
     (function() {
       "use strict";
       $(document).ready(function () {
 
-      });
+        function tableLoad (){
+            $.ajax('../db.json')
+                .done(function (movies) {
+                    for (let i=0; i<movies.length; i++) {
+                        $('#insertMovies').append
+                        (
+                            "<tr><td>" + movies[i].title +
+                            "</td><td>" + movies[i].rating +
+                            "</td><td>" + movies[i].id +
+                            "</td></tr>"
+                        )
+                    } //for loop close //
+                }) //.done function close//
+        } // tableLoad function close //
 
+          tableLoad()
+          $('#refresh').click(function (){
+              $('#insertMovies').empty();
+              tableLoad()
+          }) // refresh close //
 
-      // TODO: Take the data from inventory.json and append it to the products table
-      var  inventory = $.get("src/db.json")
-          .done(function (items) {
-            for (var i = 0; i < items.length; i++) {
-              $('#insertProducts').append(tableData(i));
+      });  // document ready close //
 
-            }
-
-            function tableData(i) {
-              return (
-                  '<tr><td>' + items [i].title +
-                  '</td><td>' + items [i].quantity +
-                  '</td><td>' + items [i].price +
-                  '</td><td>' + items[i].categories +
-                  '</td></tr>')
-            }
-
-
-            $("#btn").click(function () {
-              $('#insertProducts').empty();
-              var  inventory = $.get("data/inventory.json")
-                  .done(function (items) {
-                    for (var i = 0; i < items.length; i++) {
-                      $('#insertProducts').append(tableData(i));
-
-                    }
-
-                    function tableData(i) {
-                      return (
-                          '<tr><td>' + items [i].title +
-                          '</td><td>' + items [i].quantity +
-                          '</td><td>' + items [i].price +
-                          '</td><td>' + items[i].categories +
-                          '</td></tr>')
-                    }
-
-                  })
-            })
-
-          });
-
-
-    })();
+    })(); // iffe close //
 
